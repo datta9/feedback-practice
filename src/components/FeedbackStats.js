35e13e7eaf4911102a39023d98389eb38b30e68a
datta9 <datta.yh@gmail.com>
2022-08-05 16:57:58 +0530

    Using JSON Server

--- src/components/FeedbackStats.js
+++ src/components/FeedbackStats.js
@@ -1,26 +1,28 @@
-import {useContext} from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
 import FeedbackContext from './context/FeedbackContext';
 
-function FeedbackStats(){
-   const {feedback} = useContext(FeedbackContext)
+function FeedbackStats() {
+   const { feedback } = useContext(FeedbackContext);
 
    // Calculate avg rating
-   let average = feedback.reduce((acc, cur) => {
-      return acc + cur.rating
-   }, 0) / feedback.length
+   let average = feedback.reduce((total, item) => {
+      return total + item.rating;
+   }, 0) / feedback.length;
 
-   average = average.toFixed(1).replace(/[./]0$/, '')
+   average = average.toFixed(1).replace(/[./]0$/, '');
 
-   return(
+   return (
       <div className="feedback-stats">
          <h4>{feedback.length} Reviews</h4>
          <h4>Average rating : {isNaN(average) ? 0 : average}</h4>
+
       </div>
-   )
+   );
 }
 
 // FeedbackStats.propTypes = {
 //    feedback: PropTypes.array.isRequired
 // }
 
-export default FeedbackStats
+export default FeedbackStats;
